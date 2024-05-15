6c93cccce0f996bda36cb3ed45070091d2eeac8e
Berencsi Csaba <csaba.berencsi@gmail.com>
2024-05-15 11:26:41 +0200

    WebGL update
    .

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -8,37 +8,100 @@ using UnityEngine;
 
 public class AgentController : Agent
 {
+    // Key
     [SerializeField] Transform _keyTransform;
     [SerializeField] GameObject _keyObject;
 
+    // Door
     [SerializeField] Transform _doorTransform;
     [SerializeField] GameObject _doorObject;
-
-    [SerializeField] Transform _agentTransform;
-    Vector3 _spawnPoint = new(-20f, 1f, 23f);
-
-
-    [SerializeField] float _moveSpeed = 10f;
     [SerializeField] Animator anim;
 
+    // Agent
+    [SerializeField] Transform _agentTransform;
+    Vector3 _spawnPoint = new(-20f, 1f, 23f);
+    [SerializeField] float _moveSpeed = 5f;
+    
     const float _maxDistance = 70.71067f;
+    int _episodeCount = 0;
+
     public override void OnEpisodeBegin()
     {
         anim.ResetTrigger("KeyTrigger");
         _keyObject.SetActive(true);
         transform.localPosition = _spawnPoint;
         transform.rotation = Quaternion.Euler(0, 180f, 0f);
-        _keyObject.SetActive(true);
+
+        // All possible key locations
+        Vector3 _1stSpawnPoint = new(-8.89f, 0.5f, -9.21f);
+        Vector3 _2ndSpawnPoint = new(-23.14f, 0.5f, -22f);
+        Vector3 _3rdSpawnPoint = new(-3.08f, 0.5f, -0.1f);
+        Vector3 _4thSpawnPoint = new(7.26f, 0.5f, 14f);
+        Vector3 _5thSpawnPoint = new(22.57f, 0.5f, -17.39f);
+        Vector3 _6thSpawnPoint = new(17.32f, 0.5f, 16.84f);
+        Vector3 _7thSpawnPoint = new(5.95f, 0.5f, 14.14f);
+        Vector3 _8thSpawnPoint = new(-7.47f, 0.5f, 14.79f);
+        Vector3 _9thSpawnPoint = new(-7.47f, 0.5f, 10.01f);
+        Vector3 _10thSpawnPoint = new(-7.47f, 0.5f, 22.01f);
+        Vector3 _11thSpawnPoint = new(-22.1f, 0.5f, 8.46f);
+
+        // Select random key location for every 100th episode
+        if (_episodeCount % 100 == 0)
+        {
+            int _randomNumber = Random.Range(0, 11);
+            switch (_randomNumber)
+            {
+                case 0:
+                    _keyTransform.localPosition = _1stSpawnPoint;
+                    break;
+                case 1:
+                    _keyTransform.localPosition = _2ndSpawnPoint;
+                    break;
+                case 2:
+                    _keyTransform.localPosition = _3rdSpawnPoint;
+                    break;
+                case 3:
+                    _keyTransform.localPosition = _4thSpawnPoint;
+                    break;
+                case 4:
+                    _keyTransform.localPosition = _5thSpawnPoint;
+                    break;
+                case 5:
+                    _keyTransform.localPosition = _6thSpawnPoint;
+                    break;
+                case 6:
+                    _keyTransform.localPosition = _7thSpawnPoint;
+                    break;
+                case 7:
+                    _keyTransform.localPosition = _8thSpawnPoint;
+                    break;
+                case 8:
+                    _keyTransform.localPosition = _9thSpawnPoint;
+                    break;
+                case 9:
+                    _keyTransform.localPosition = _10thSpawnPoint;
+                    break;
+                case 10:
+                    _keyTransform.localPosition = _11thSpawnPoint;
+                    break;
+                default:
+                    break;
+            }
+        }
+        _episodeCount++;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition.x);
         sensor.AddObservation(transform.localPosition.z);
+
         sensor.AddObservation(_keyTransform.localPosition.x);
         sensor.AddObservation(_keyTransform.localPosition.z);
-    }
 
+        sensor.AddObservation(_doorTransform.localPosition.x);
+        sensor.AddObservation(_doorTransform.localPosition.z);
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
         var actionTaken = actions.ContinuousActions;
@@ -47,17 +110,19 @@ public class AgentController : Agent
         float _actionSteering = actionTaken[1];
         transform.Translate(_actionSpeed * Vector3.forward * _moveSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up, _actionSteering * 180f * Time.deltaTime);
-        
+
+        float _keyToAgentDist = Vector3.Distance(_keyTransform.localPosition, transform.localPosition) / _maxDistance;
+        float _doorToAgentDist = Vector3.Distance(_doorTransform.localPosition, transform.localPosition) / _maxDistance;
+
         if (_keyObject.activeSelf)
         {
-            float distance_scaled = Vector3.Distance(_keyTransform.localPosition, _agentTransform.localPosition) / _maxDistance;
-            AddReward(-distance_scaled / 10);
+            AddReward(-_keyToAgentDist / 10);
         }
         else
         {
-            float distance_scaled = Vector3.Distance(_doorTransform.localPosition, _agentTransform.localPosition) / _maxDistance;
-            AddReward(-distance_scaled / 10);
+            AddReward(-_doorToAgentDist / 10);
         }
+
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -92,20 +157,20 @@ public class AgentController : Agent
     {
         if (collider.CompareTag("Key"))
         {
-            AddReward(2);
+            AddReward(2f);
             anim.SetTrigger("KeyTrigger");
             _keyObject.SetActive(false);
 
         }
         if (collider.CompareTag("Door"))
         {
-            AddReward(4);
+            AddReward(4f);
             EndEpisode(); 
         }
 
         if (collider.CompareTag("Wall"))
         {
-            AddReward(-1);
+            AddReward(-0.01818f);
             EndEpisode();
         }
     }
